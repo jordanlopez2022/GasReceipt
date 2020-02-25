d4d952686a8c9aaf9cfddc61efdbb9d3cf4b3be2
LOPEZ <jordan.lopez.2022@student.lhschools.org>
2020-02-25 13:42:53 -0500

    GasReceiptSourceControl

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,28 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("+------------------------------+");
+	System.out.println("|                              |");
+	System.out.println("| Lopez Gas Station            |");
+	System.out.println("| 8098 Biltmore Dr             |");
+    System.out.println("| Blacklick                    |");
+    System.out.println("| Ohio 43004                   |");
+	System.out.println("| Date            Time         |");
+	System.out.println("| 26JAN2020       10:00 PM     |");
+	System.out.println("| Invoice number#     123214   |");
+	System.out.println("| Auth#                        |");
+	System.out.println("| Mastercard                   |");
+	System.out.println("| Account Number               |");
+	System.out.println("| **************19             |");
+	System.out.println("| Pump     Product      $/G    |");
+	System.out.println("| 01     UNLD         $2.9     |");
+	System.out.println("| Gallons  Fuel    Total       |");
+    System.out.println("| 20.69       $    60.00       |");
+    System.out.println("|                              |");
+    System.out.println("| Thank you for stopping by    |");
+    System.out.println("Lopez Gas Station              |");
+    System.out.println("+------------------------------+");
+    }
+}
